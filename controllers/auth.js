e76f8bfc1f3945666f64db1c9a26403ad890b512
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-22 16:48:05 -0400

    Set up controllers

---
+++ controllers/auth.js
@@ -0,0 +1,5 @@
+exports.createOrUpdateUser = (req, res) => {
+  res.json({
+    data: "You hit the create-or-update-user API end point of eShop"
+  })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
+// Import controllers
+const {createOrUpdateUser} = require("../controllers/auth");
 
   // Routes
-  router.get('/create-or-update-user', (req,res) => {
-    res.json({
-      data:"You hit eShop's create-or-update API endpoint"
-    });
-  });
+  router.get('/create-or-update-user', createOrUpdateUser);
 
+  // Export router so
+  // it can be used in the server.js
+  // This is similar to default export
+  // in that when importing the naming
+  // is flexible
   module.exports = router;
+
+  // Compare with:
+  // exports.createOrUpdateUser = (req, res) => {
+  //   res.json({
+  //     data: "You hit the create-or-update-user API end point of eShop"
+  //   })
+  // }
+  // which acts similar to name exports
+  // and therefore should be importedd as such
