9fde0e736f490407d6f9803f6a623fed879f725c
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-23 16:44:31 -0400

    Create or update user on login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,42 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-    data: "You hit the create-or-update-user API end point of eShop"
-  })
-}
+// Bring in user model
+const User = require("../models/user");
+
+// Create or update user information based on verification
+// Obtained from running authCheck middleware
+// which in turn utliizes firebase admin
+// user info is available on the user
+// because of the authCheck middleware
+exports.createOrUpdateUser = async (req, res) => {
+  // authCheck middleware runs first
+  // Destructure values from autCheck
+  const { name, picture, email } = req.user;
+  // If user already exits in the database, update
+  // Find user based on email {email:email} and update name and picture
+  // {new:true} returns updated user information / prevents returning the
+  // older information
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name, picture },
+    { new: true }
+  );
+
+  // res.json({
+  //   data: "You hit the create-or-update-user API end point of eShop"
+  // })
+
+  // If update was successful, true is returned
+
+  if (user) {
+    console.log("User updated", user);
+    res.json(user);
+    // If user is not found create the user
+  } else {
+    const newUser = await new User({
+      email,
+      name,
+      picture,
+    }).save();
+    console.log("User Created", newUser);
+    res.json(newUser);
+  }
+};
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,23 @@ const admin = require('../firebase');
 // Recieve token in request headers
 // on user log in from frontend
 // check whether the token is valid
-exports.authCheck = (req, res, next) => {
-  console.log(req.headers);
-  next();
+exports.authCheck = async (req, res, next) => {
+  try {
+    // console.log('Hey', req.headers.authtoken);
+    
+    // Verify token using firebase admin
+    const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken)
+    console.log('Firebase user in authCheck', firebaseUser);
+    // Make user available in the createOrUpdate controller method
+    // So it can be stored in the database
+    // One way to make it available is by adding it
+    // to the express's request object
+    req.user = firebaseUser;
+    next();
+
+  } catch(err) {
+    res.status(401).json({
+      err: "Invalid or expired token"
+    })
+  }
 }
