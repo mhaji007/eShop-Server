2b91d623b465ffea2d5154afbbb4600907a0f334
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-02 20:27:02 -0500

    Set up image upload and delete endpoints

---
+++ controllers/cloudinary.js
@@ -0,0 +1,48 @@
+const cloudinary = require("cloudinary");
+
+// Cloudinary config
+// enables using env variables
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Endpoint for uploading images to cloudinary
+
+exports.upload = async (req, res) => {
+  // cloudinary.uploadder.upload is available via cloudinary package
+  // Since we are using binary data (json) and not form data
+  // image is accessed via req.body.image
+  // If form data was used, image could be accessed via
+  // req.files.file.path
+  // result contains the uploaded image url
+  let result = await cloudinary.uploader.upload(req.body.image, {
+    // Publicly visible id
+    public_id: `${Date.now()}`,
+    // Automatically use image format as resource type
+    // e.g., jpeg, png, etc.
+    resource_type: "auto",
+  });
+  // Send image url from cloudinary to the frontend
+  res.json({
+    public_id: result.public_id,
+    url: result.secure_url,
+  });
+};
+
+// Endpoint for removing images
+
+exports.remove = (req, res) => {
+  // Send image id as the identifier
+  // of which image is to be deleted
+  let image_id = req.body.public_id;
+
+  cloudinary.uploader.destroy(image_id, (err, result) => {
+    if (err) return res.json({ success: false, err });
+    // res.status(200).send("ok")
+    // status 200 is the default status
+    // and here could be done away with
+    res.send("ok");
+  });
+};
