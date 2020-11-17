cf06b91fb5094099071a0e2e5ff5935e2a10bc0d
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-17 16:00:59 -0500

    Implement filter by shipping, color and brand

--- controllers/product.js
+++ controllers/product.js
@@ -396,11 +396,39 @@ const handleSubcategory = async (req, res, subcategory) => {
   res.json(products);
 };
 
+const handleShipping = async (req, res, shipping) => {
+  const products = await Product.find({ shipping })
+    .populate("category", "_id name")
+    .populate("subs", "_id name")
+    .populate("postedBy", "_id name")
+    .exec();
+
+  res.json(products);
+};
+const handleColor = async (req, res, color) => {
+  const products = await Product.find({ color })
+    .populate("category", "_id name")
+    .populate("subs", "_id name")
+    .populate("postedBy", "_id name")
+    .exec();
+
+  res.json(products);
+};
+const handleBrand = async (req, res, brand) => {
+  const products = await Product.find({ brand})
+    .populate("category", "_id name")
+    .populate("subs", "_id name")
+    .populate("postedBy", "_id name")
+    .exec();
+
+  res.json(products);
+};
+
 exports.searchFilters = async (req, res) => {
   // Destructure search query from body
   // search term is sent in from frontend
   // in the form of { query: text } in the body
-  const { query, price, category, stars, subcategory } = req.body;
+  const { query, price, category, stars, subcategory, shipping, color, brand } = req.body;
 
   if (query) {
     console.log("query  ---> ", query);
@@ -423,5 +451,17 @@ exports.searchFilters = async (req, res) => {
   if (subcategory) {
     console.log("stars ---> ", subcategory);
     await handleSubcategory(req, res, subcategory);
+  }
+  if (shipping) {
+    console.log("shipping ---> ", shipping);
+    await handleShipping(req, res, shipping);
+  }
+  if (brand) {
+    console.log("shipping ---> ", brand);
+    await handleBrand(req, res, brand);
+  }
+  if (color) {
+    console.log("shipping ---> ", color);
+    await handleColor(req, res, color);
   }
 };
