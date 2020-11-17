f3297e1103c12b5bdd641df0c574dc92e9d0290e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-16 23:41:36 -0500

    Implement filter by categories

--- controllers/product.js
+++ controllers/product.js
@@ -312,11 +312,26 @@ const handlePrice = async (req, res, price) => {
   }
 };
 
+const handleCategory = async (req, res, category) => {
+  try {
+    let products = await Product.find({
+      category,
+    })
+      .populate("category", "_id name")
+      .populate("subs", "_id name")
+      .populate("postedBy", "_id name")
+      .exec();
+    res.json(products);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 exports.searchFilters = async (req, res) => {
   // Destructure search query from body
   // search term is sent in from frontend
   // in the form of { query: text } in the body
-  const { query, price } = req.body;
+  const { query, price, category } = req.body;
 
   if (query) {
     console.log("query", query);
@@ -326,5 +341,10 @@ exports.searchFilters = async (req, res) => {
   if (price !== undefined) {
     console.log("price", price);
     await handlePrice(req, res, price);
+  }
+
+  if (category) {
+    console.log("category", category);
+    await handleCategory(req, res, category);
   }
 };
