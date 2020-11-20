901b60926e0a7474343da85add0d3d26d3af12be
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-20 14:54:59 -0500

    Set up coupon schema

---
+++ models/coupon.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const couponSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: true,
+      uppercase: true,
+      required: "Name is required",
+      minlength: [6, "Name must be at least 6 characters long"],
+      maxlenght: [12, "Name cannot exceed 12 characters"],
+    },
+    expiry: {
+      type: Date,
+      required: true,
+    },
+    // How much percentage off
+    discount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", couponSchema);
