0c3b0839fc31cd753c20c0304091c3472c9cde62
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-25 14:26:49 -0500

    Modify stripe controller to charge total after discount

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -5,27 +5,46 @@ const Coupon = require("../models/coupon");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
-  //TODO:
+  const { couponApplied } = req.body;
+
   // Apply coupon
   // Calculcate price
 
   // Get the currenlty logged-in user cart and
   // extract the amount to be charged
 
-
   // Find user
   const user = await User.findOne({ email: req.user.email }).exec();
   // Get user cart total
-  const { cartTotal } = await Cart.findOne({ orderedBy: user._id }).exec();
+  const { cartTotal, totalAfterDiscount } = await Cart.findOne({
+    orderedBy: user._id,
+  }).exec();
 
-  console.log("CART TOTAL CHARGED", cartTotal);
+  console.log(
+    "CART total before discount",
+    cartTotal,
+    "After Discount",
+    totalAfterDiscount
+  );
+
+  let finalAmount = 0;
+
+  if (couponApplied && totalAfterDiscount) {
+    finalAmount = totalAfterDiscount * 100;
+  } else {
+    finalAmount = cartTotal * 100;
+  }
+
   // create payment intent with order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: cartTotal * 100,
+    amount: finalAmount,
     currency: "usd",
   });
 
   res.send({
     clientSecret: paymentIntent.client_secret,
+    cartTotal,
+    totalAfterDiscount,
+    payable: finalAmount,
   });
 };
